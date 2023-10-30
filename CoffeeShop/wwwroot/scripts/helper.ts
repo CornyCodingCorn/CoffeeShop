a85ab20ec8cc4d7a33ff9767f8f96425f08e88fe
CornyCodingCorn <nguyenpham0214@gmail.com>
2023-10-30 11:55:40 +0700

    Reconfigure how js works

---
+++ CoffeeShop/wwwroot/scripts/helper.ts
@@ -0,0 +1,32 @@
+// @ts-ignore
+const DotNet = (await import("@microsoft/dotnet-js-interop").catch(_ => {}))?.DotNet;
+// @ts-ignore
+type DotNetObject = DotNet.DotNetObject
+
+export function addVisibleTriggers(ref: Element, dotnetObj: DotNetObject, onVisibleFunc: string, onInvisibleFunc: string) {
+    let observer = new IntersectionObserver(async (entries) => {
+        if (entries[0].isIntersecting === true) {
+            if (onVisibleFunc !== "") 
+                await dotnetObj.invokeMethodAsync(onVisibleFunc);
+        } else {
+            if (onInvisibleFunc !== "") 
+                await dotnetObj.invokeMethodAsync(onInvisibleFunc);
+        }
+    }, {threshold: [0]})
+    
+    observer.observe(ref);
+    return observer;
+}
+
+export function removeVisibleTriggers(observer: IntersectionObserver) {
+    observer.disconnect();
+}
+
+export function capturePointer(ref: Element, id: number) {
+    console.log("Logging from capture pointer");
+    ref.setPointerCapture(id);
+}
+
+export function releasePointer(ref: Element, id: number) {
+    ref.releasePointerCapture(id);
+}
