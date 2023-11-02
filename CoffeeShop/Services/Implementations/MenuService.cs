5c38a713db04a88cce1cad87be09d228faa292d8
CornyCodingCorn <nguyenpham0214@gmail.com>
2023-11-02 22:34:30 +0700

    Finished menu and fix alot of bugs

--- CoffeeShop/Services/Implementations/MenuService.cs
+++ CoffeeShop/Services/Implementations/MenuService.cs
@@ -36,6 +36,7 @@ public class MenuService : IMenuService
         {
             Name = "Café au lait",
             Price = 20,
+            Description = _itemDes,
             ImageUrl =
                 "http://clipart-library.com/images/pc7r5rbLi.jpg https://empire-s3-production.bobvila.com/articles/wp-content/uploads/2020/08/best_coffee_mug.jpg https://media.istockphoto.com/photos/cup-of-coffee-picture-id517151571?k=6&m=517151571&s=612x612&w=0&h=pm2doj5opQSCG7vGpddVPQopuhcHlpoY2rahSDsQpPg="
         },
@@ -43,6 +44,7 @@ public class MenuService : IMenuService
         {
             Name = "Black coffee",
             Price = 20,
+            Description = _itemDes,
             ImageUrl =
                 "https://empire-s3-production.bobvila.com/articles/wp-content/uploads/2020/08/best_coffee_mug.jpg https://trircoffee.com/images/homebottom/4.jpg"
         },
@@ -50,6 +52,7 @@ public class MenuService : IMenuService
         {
             Name = "Café con leche",
             Price = 20,
+            Description = _itemDes,
             ImageUrl =
                 "https://trircoffee.com/images/homebottom/4.jpg https://wgntv.com/wp-content/uploads/sites/5/2016/09/thinkstockphotos-521696656-e1475156855762.jpg?w=900"
         },
@@ -57,6 +60,7 @@ public class MenuService : IMenuService
         {
             Name = "Flat White",
             Price = 20,
+            Description = _itemDes,
             ImageUrl =
                 "https://wgntv.com/wp-content/uploads/sites/5/2016/09/thinkstockphotos-521696656-e1475156855762.jpg?w=900 https://www.hdwallpaper.nu/wp-content/uploads/2017/03/coffee-7.jpg"
         },
@@ -64,6 +68,7 @@ public class MenuService : IMenuService
         {
             Name = "Latte Macchiato",
             Price = 20,
+            Description = _itemDes,
             ImageUrl =
                 "https://www.hdwallpaper.nu/wp-content/uploads/2017/03/coffee-7.jpg https://sundun.com/images/single2.jpg"
         },
@@ -71,6 +76,7 @@ public class MenuService : IMenuService
         {
             Name = "Vienna Coffee",
             Price = 20,
+            Description = _itemDes,
             ImageUrl =
                 "https://sundun.com/images/single2.jpg https://johnthebodyman.com/wp-content/uploads/2014/05/Bodyman-coffee-in-cup-1024x1024.jpg https://media.istockphoto.com/photos/cup-of-coffee-picture-id517151571?k=6&m=517151571&s=612x612&w=0&h=pm2doj5opQSCG7vGpddVPQopuhcHlpoY2rahSDsQpPg="
         },
@@ -78,6 +84,7 @@ public class MenuService : IMenuService
         {
             Name = "Ristretto",
             Price = 20,
+            Description = _itemDes,
             ImageUrl =
                 "https://johnthebodyman.com/wp-content/uploads/2014/05/Bodyman-coffee-in-cup-1024x1024.jpg https://media.istockphoto.com/photos/cup-of-coffee-picture-id517151571?k=6&m=517151571&s=612x612&w=0&h=pm2doj5opQSCG7vGpddVPQopuhcHlpoY2rahSDsQpPg="
         },
@@ -85,10 +92,29 @@ public class MenuService : IMenuService
         {
             Name = "Affogato",
             Price = 20,
+            Description = _itemDes,
             ImageUrl =
                 "https://media.istockphoto.com/photos/cup-of-coffee-picture-id517151571?k=6&m=517151571&s=612x612&w=0&h=pm2doj5opQSCG7vGpddVPQopuhcHlpoY2rahSDsQpPg="
         },
     };
+
+    private static string _itemDes =
+        /*language=html*/"""
+                         This coffee is a <strong>bold and robust</strong> 
+                         blend of beans sourced from the <em>lush and verdant</em> 
+                         hills of Colombia. The aroma is <strong>intense and earthy</strong>, 
+                         with notes of <em>dark chocolate</em> and <em>caramel</em>. 
+                         The flavor is <strong>full-bodied and complex</strong>, with hints of <em>spice</em> and <em>nutmeg</em>. 
+                         The finish is <strong>smooth and lingering</strong>, with a subtle hint of <em>smokiness</em>. 
+                         This coffee is perfect for those who enjoy a strong, flavorful brew that packs a punch.
+                         This coffee is a <strong>bold and robust</strong> 
+                         blend of beans sourced from the <em>lush and verdant</em> 
+                         hills of Colombia. The aroma is <strong>intense and earthy</strong>, 
+                         with notes of <em>dark chocolate</em> and <em>caramel</em>. 
+                         The flavor is <strong>full-bodied and complex</strong>, with hints of <em>spice</em> and <em>nutmeg</em>. 
+                         The finish is <strong>smooth and lingering</strong>, with a subtle hint of <em>smokiness</em>. 
+                         This coffee is perfect for those who enjoy a strong, flavorful brew that packs a punch.
+                         """;
     
     public async Task<IEnumerable<MenuCategory>> LoadCategories()
     {
@@ -118,5 +144,11 @@ public class MenuService : IMenuService
         }
 
         return results;
+    }
+
+    public async Task<bool> OrderItem(MenuItem item)
+    {
+        await Task.Delay(3000);
+        return Random.Shared.Next(0, 1000) % 2 == 0;
     }
 }
--- CoffeeShop/Services/Types/MenuItem.cs
+++ CoffeeShop/Services/Types/MenuItem.cs
@@ -7,11 +7,12 @@ public class MenuItem
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string[] ImageUrls { get; set; }
+    public string Description { get; set; }
     public decimal Price { get; set; }
     
     public MenuItem(ItemDto dto)
     {
         // Split of a single string into multiple url
-        (Id, Name, ImageUrls, Price) = (dto.Id, dto.Name, dto.ImageUrl.Split(" "), dto.Price);
+        (Id, Name, ImageUrls, Price, Description) = (dto.Id, dto.Name, dto.ImageUrl.Split(" "), dto.Price, dto.Description);
     }
 }
