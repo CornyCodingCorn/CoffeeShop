a885ca4f6916a1470f9fbd52f7a19f14c72c8191
CornyCodingCorn <nguyenpham0214@gmail.com>
2023-11-05 17:11:59 +0700

    Add shops view

--- CoffeeShop/DTOs/AddressDto.cs
+++ CoffeeShop/DTOs/AddressDto.cs
@@ -1,10 +1,12 @@
 namespace CoffeeShop.DTOs;
 
-public class AddressDto
+public struct AddressDto
 {
     public string City { get; set; } = "A city name";
     public string District { get; set; } = "A district name";
     public string Ward { get; set; } = "Ward's name";
     public string Street { get; set; } = "Street's name";
     public string Number { get; set; } = "Building's number";
+    
+    public AddressDto() { }
 }
---
+++ CoffeeShop/Services/Interfaces/IShopService.cs
@@ -0,0 +1,10 @@
+using CoffeeShop.Services.Types;
+
+namespace CoffeeShop.Services.Interfaces;
+
+public interface IShopService
+{
+    public Task<IEnumerable<ShopInfo>> LoadShops();
+    public Task<IEnumerable<ShopInfo>> LoadShops(string city);
+    public Task<IEnumerable<ShopInfo>> LoadShops(string city, string district);
+}
